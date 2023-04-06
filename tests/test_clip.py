007af87b8a77f98538645b9aac63d8020c4f5e2f
Divided by Zer0 <mail@dbzer0.com>
2023-04-07 00:37:18 +0200

    clip tests

--- tests/test_clip.py
+++ tests/test_clip.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 from hordelib.horde import HordeLib
 from hordelib.shared_model_manager import SharedModelManager
-from hordelib.safety_checker import is_image_nsfw
+from hordelib.clip.interrogate import Interrogator
 
 
 class TestHordePostProcessing:
@@ -15,28 +15,30 @@ class TestHordePostProcessing:
         self.default_model_manager_args = {
             # aitemplate
             # "blip": True,
-            # "clip": True,
+            "clip": True,
             # "codeformer": True,
             # "compvis": True,
             # "controlnet": True,
             # "diffusers": True,
             # "esrgan": True,
             # "gfpgan": True,
-            "safety_checker": True,
+            # "safety_checker": True,
         }
         self.image = Image.open("db0.jpg") 
         SharedModelManager.loadModelManagers(**self.default_model_manager_args)
         assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("ViT-L/14")
         yield
         del self.horde
         SharedModelManager._instance = None
         SharedModelManager.manager = None
 
-    def test_safety_checker_with_preload(self):
-        SharedModelManager.manager.load("safety_checker")
-        assert SharedModelManager.manager.safety_checker.is_model_loaded("safety_checker") is True
-        assert is_image_nsfw(self.image) is False
-
-    def test_safety_checker_without_preload(self):
-        assert SharedModelManager.manager.safety_checker.is_model_loaded("safety_checker") is False
-        assert is_image_nsfw(self.image) is False
+    def test_clip_similarities(self):
+        assert SharedModelManager.manager.clip.is_model_loaded("ViT-L/14") is True
+        word_list = ["outlaw", "explosion", "underwater"]
+        interrogator = Interrogator(SharedModelManager.manager.loaded_models["ViT-L/14"])
+        similarity_result = interrogator(image=self.image, text_array=word_list, similarity=True)
+        assert "default" in similarity_result
+        assert similarity_result["default"]["outlaw"] > 0.15
+        assert similarity_result["default"]["explosion"] > 0.15
+        assert similarity_result["default"]["underwater"] < 0.15
