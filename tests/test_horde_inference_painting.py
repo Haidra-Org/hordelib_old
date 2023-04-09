b96d85e6ebc83657969cd0a1d339b1ed1a33f07b
Divided by Zer0 <mail@dbzer0.com>
2023-04-09 17:23:39 +0200

    inpainting tests

---
+++ tests/test_horde_inference_painting.py
@@ -0,0 +1,59 @@
+# test_horde.py
+import pytest
+from PIL import Image
+
+from hordelib.horde import HordeLib
+from hordelib.shared_model_manager import SharedModelManager
+
+
+class TestHordeInference:
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        self.horde = HordeLib()
+
+        self.default_model_manager_args = {
+            # aitemplate
+            # "blip": True,
+            # "clip": True,
+            # "codeformer": True,
+            "compvis": True,
+            # "controlnet": True,
+            # "diffusers": True,
+            # "esrgan": True,
+            # "gfpgan": True,
+            # "safety_checker": True,
+        }
+        SharedModelManager.loadModelManagers(**self.default_model_manager_args)
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("stable_diffusion_inpainting")
+        yield
+        del self.horde
+        SharedModelManager._instance = None
+        SharedModelManager.manager = None
+
+    def test_inpainting_alpha_mask(self):
+        data = {
+            "sampler_name": "k_dpmpp_2m",
+            "cfg_scale": 7.5,
+            "denoising_strength": 0.72,
+            "seed": 836913938046008,
+            "height": 512,
+            "width": 512,
+            "karras": False,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": "canny",
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a mecha robot with a long tail sitting on a bench",
+            "ddim_steps": 20,
+            "n_iter": 1,
+            "model": "stable_diffusion_inpainting",
+            "source_image": Image.open("images/test_inpaint_alpha.png"),
+            "source_processing": "inpainting",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.basic_inference(data)
+        assert pil_image is not None
+        pil_image.save("images/horde_inpainting_alpha_mask.webp", quality=90)
