121c057998c6ae329f52ed9b3ca3acaba15abe2d
Jug <jugdev@proton.me>
2023-04-11 16:45:07 +0100

    build: add release mode flag

--- hordelib/consts.py
+++ hordelib/consts.py
@@ -1,4 +1,6 @@
 # consts.py
+import os
+from hordelib.config_path import get_hordelib_path
 
 # ComfyUI exact version we need
 COMFYUI_VERSION = "c767e9426ae81bed4f52c7be0625f0efc4cbe16b"
@@ -20,3 +22,6 @@ DEFAULT_MODEL_MANAGERS = {
 REMOTE_MODEL_DB = (
     "https://raw.githubusercontent.com/db0/AI-Horde-image-model-reference/main/"
 )
+
+# A flag for if this is a pypi release or a git dev mode
+RELEASE_VERSION = os.path.exists(os.path.join(get_hordelib_path(), "_version.py"))
--- hordelib/initialisation.py
+++ hordelib/initialisation.py
@@ -4,11 +4,19 @@ import sys
 
 from loguru import logger
 from hordelib import install_comfy
-from hordelib.config_path import set_system_path
-from hordelib.consts import COMFYUI_VERSION, DEFAULT_MODEL_MANAGERS
+from hordelib.config_path import set_system_path, get_hordelib_path
+from hordelib.consts import COMFYUI_VERSION, DEFAULT_MODEL_MANAGERS, RELEASE_VERSION
 
 
 def initialise():
+    # If developer mode, don't permit some things
+    if not RELEASE_VERSION and " " in get_hordelib_path():
+        # Our runtime patching can't handle this
+        raise Exception(
+            "Do not run this project in developer mode from a path that "
+            "contains spaces in directory names."
+        )
+
     # Ensure we have ComfyUI
     logger.debug("Clearing command line args in sys.argv before ComfyUI load")
     sys_arg_bkp = sys.argv.copy()
