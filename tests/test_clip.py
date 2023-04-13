bd86f43e1c34fd0cba5f91b8d7df39e8f800a875
Jug <jugdev@proton.me>
2023-04-13 10:35:31 +0100

    fix: Revert "feat: Clip Rankings (#117)"
    This reverts commit 169f7512479bcaddb5e42cdc49465b8169dc0280.

--- tests/test_clip.py
+++ tests/test_clip.py
@@ -8,11 +8,11 @@ from hordelib.shared_model_manager import SharedModelManager
 
 
 class TestHordeClip:
-    @pytest.fixture(autouse=True, scope="class")
+    @pytest.fixture(autouse=True)
     def setup_and_teardown(self):
-        TestHordeClip.horde = HordeLib()
+        self.horde = HordeLib()
 
-        TestHordeClip.default_model_manager_args = {
+        self.default_model_manager_args = {
             # aitemplate
             # "blip": True,
             "clip": True,
@@ -24,17 +24,17 @@ class TestHordeClip:
             # "gfpgan": True,
             # "safety_checker": True,
         }
-        TestHordeClip.image = Image.open("images/test_db0.jpg")
+        self.image = Image.open("images/test_db0.jpg")
         SharedModelManager.loadModelManagers(**self.default_model_manager_args)
         assert SharedModelManager.manager is not None
         SharedModelManager.manager.load("ViT-L/14")
         yield
-        del TestHordeClip.horde
+        del self.horde
         SharedModelManager._instance = None
         SharedModelManager.manager = None
 
     def test_clip_similarities(self):
-        assert SharedModelManager.manager.is_model_loaded("ViT-L/14") is True
+        assert SharedModelManager.manager.clip.is_model_loaded("ViT-L/14") is True
         word_list = ["outlaw", "explosion", "underwater"]
         model_info = SharedModelManager.manager.loaded_models["ViT-L/14"]
         interrogator = Interrogator(model_info)
@@ -47,15 +47,3 @@ class TestHordeClip:
         assert similarity_result["default"]["outlaw"] > 0.15
         assert similarity_result["default"]["explosion"] > 0.15
         assert similarity_result["default"]["underwater"] < 0.15
-
-    def test_clip_rankings(self):
-        assert SharedModelManager.manager.is_model_loaded("ViT-L/14") is True
-        model_info = SharedModelManager.manager.loaded_models["ViT-L/14"]
-        interrogator = Interrogator(model_info)
-        ranking_result = interrogator(
-            image=self.image,
-            rank=True,
-        )
-        assert type(ranking_result) is dict
-        assert "artists" in ranking_result
-        assert "mediums" in ranking_result
