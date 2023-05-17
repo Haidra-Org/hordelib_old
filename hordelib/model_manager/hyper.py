9997750550dd6747e1dd7eeef3be12379d431757
tazlin <tazlin.on.github@gmail.com>
2023-05-17 13:51:20 -0400

    fix: include diffusers until larger patch

--- hordelib/model_manager/hyper.py
+++ hordelib/model_manager/hyper.py
@@ -299,7 +299,7 @@ class ModelManager:
 
     def get_available_models_by_types(self, mm_include: list[str] | None = None, mm_exclude: list[str] | None = None):
         if not mm_include and not mm_exclude:
-            mm_include = ["compvis"]
+            mm_include = ["compvis", "diffusers"]
         return self.get_available_models(mm_include, mm_exclude)
 
     def count_available_models_by_types(
