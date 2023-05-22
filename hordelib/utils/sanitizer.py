c61efc74269e41a00941f33069c51686bd1ab032
db0 <mail@dbzer0.com>
2023-05-22 16:13:13 +0200

    wip

---
+++ hordelib/utils/sanitizer.py
@@ -0,0 +1,27 @@
+
+from unidecode import unidecode
+import string
+import re
+
+class Sanitizer():
+        
+    VERSION_REMOVER = re.compile(r'(v[0-9][0-9.]*)$')
+
+    @staticmethod
+    def sanitise_model_name(name):
+        """Ensure we store model names in ascii"""
+        # We just remove exotic unicode characters
+        return unidecode(name)
+
+    @staticmethod
+    def sanitise_filename(filename):
+        """Don't allow crazy filenames, there are a lot"""
+        # First remove exotic unicode characters
+        filename = unidecode(filename)
+        # Now exploit characters
+        valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+        return "".join(c for c in filename if c in valid_chars)
+
+    @staticmethod
+    def remove_version(string):
+        return Sanitizer.VERSION_REMOVER.sub(r'', string)
